0c549e17f106a80685fa11f94ded5f5554369189
Gallerand <115129377+Gallerand@users.noreply.github.com>
2022-12-28 01:47:42 -0700

    write a simple javascript function

---
+++ index.js
@@ -0,0 +1,13 @@
+function myInformation(Name, Height, Country) {
+  alert(
+    "Name: Randy Gallegos\n" +
+      "Height: 6 Feet\n" +
+      "Country: United States of America"
+  );
+}
+
+myInformation(
+  "Name: Randy Gallegos",
+  "Height: 6 Feet",
+  "Country: United States of America"
+);
